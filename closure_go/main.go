e89eb5c8dbc38dac896eaf7daaa200196173703a
souvikjs01 <souvik741156@gmail.com>
2024-12-22 13:17:38 +0530

    closure depth

--- closure_go/main.go
+++ closure_go/main.go
@@ -2,14 +2,28 @@ package main
 
 import "fmt"
 
-func counter() func() int {
-	var count int = 0
+// func counter() func() int {
+// 	var count int = 0
 
-	return func() int {
-		return count + 1
+// 	return func() int {
+// 		return count + 1
+// 	}
+// }
+// func main() {
+// 	increment := counter()
+// 	fmt.Println(increment())
+// }
+
+func adder(x int) func(y int) int {
+	return func(y int) int {
+		return x + y
 	}
 }
+
 func main() {
-	increment := counter()
-	fmt.Println(increment())
+	add5 := adder(5)
+	fmt.Println(add5(3))
+
+	add10 := adder(10)
+	fmt.Println(add10(5))
 }
