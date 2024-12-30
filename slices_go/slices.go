3780d9fcba0bb3476d129aa7945490d3d6b3dcfb
souvikjs01 <souvik741156@gmail.com>
2024-12-30 18:28:34 +0530

    slices

--- slices_go/slices.go
+++ slices_go/slices.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 // slice
@@ -32,14 +31,14 @@ func main() {
 	// fmt.Println(len(nums))
 
 	// ---copy-----
-	var nums = make([]int, 0, 5)
-	nums = append(nums, 4)
-	nums = append(nums, 3)
-	var num2 = make([]int, len(nums))
+	// var nums = make([]int, 0, 5)
+	// nums = append(nums, 4)
+	// nums = append(nums, 3)
+	// var num2 = make([]int, len(nums))
 
 	// copy function:
-	copy(num2, nums)
-	fmt.Println(nums, num2)
+	// copy(num2, nums)
+	// fmt.Println(nums, num2)
 
 	// --------slice operator---------
 	// var arr = []int{1, 2, 3, 4}
@@ -47,12 +46,39 @@ func main() {
 	// fmt.Println(arr[2:4])
 
 	// slice compare
-	var arr1 = []int{1, 2, 6}
-	var arr2 = []int{1, 2, 3}
-	var ans = slices.Equal(arr1, arr2)
-	fmt.Println(ans)
+	// var arr1 = []int{1, 2, 6}
+	// var arr2 = []int{1, 2, 3}
+	// var ans = slices.Equal(arr1, arr2)
+	// fmt.Println(ans)
 
 	// 2d array:
-	var _arr = [][]int{{3, 4}, {8, 9}}
-	fmt.Println(_arr)
+	// var _arr = [][]int{{3, 4}, {8, 9}}
+	// fmt.Println(_arr)
+
+	//--------------------------------
+	// slc := []int{10, 23, 26, 29, 16, 27}
+	// fmt.Println(slc[1:5])
+	// fmt.Println(slc[1:6])
+
+	//-----------------------------------
+	// slice := make([]int, 3, 5)
+	// slice[0] = 1
+	// slice[1] = 2
+	// slice[2] = 3
+	// slice = append(slice, 4)
+	// slice = append(slice, 5)
+	// slice = append(slice, 6)
+
+	// fmt.Println(len(slice)) // 3
+	// fmt.Println(cap(slice)) // 5
+	// for k, v := range slice {
+	// 	fmt.Printf("slice[%d] = %d\n", k, v)
+	// }
+
+	//----------------------------------------
+	slice := make([]int, 3, 6) // Preallocate for 100 elements
+	for _, v := range slice {
+		fmt.Println("value: ", v)
+	}
+
 }
