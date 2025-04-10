30efc59b365c110e0390f211be02ea9af928329e
souvikjs01 <souvik741156@gmail.com>
2025-04-10 22:48:19 +0530

    advanced go

--- go_routines/main.go
+++ go_routines/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+/*
 func sayHello() {
 	fmt.Println("say hello to everyone")
 	time.Sleep(1 * time.Second)
@@ -22,4 +24,71 @@ func main() {
 
 	// wait a moment to allow goroutine to finish:
 	time.Sleep(1 * time.Second)
+}
+
+*/
+
+// example :2
+
+type Message struct {
+	chats   []string
+	friends []string
+}
+
+func main() {
+	now := time.Now()
+	id := getUserIdByName("john")
+	fmt.Println(id)
+
+	wg := &sync.WaitGroup{}
+	ch := make(chan *Message, 2)
+
+	wg.Add(2)
+
+	go getUserChats(id, ch, wg)
+	go getUserFriends(id, ch, wg)
+
+	wg.Wait()
+	close(ch)
+
+	for data := range ch {
+		fmt.Println(data)
+	}
+	fmt.Println(time.Since(now))
+}
+
+func getUserIdByName(name string) string {
+	time.Sleep(2 * time.Second)
+
+	return fmt.Sprintf("%s-2", name)
+}
+
+func getUserChats(id string, ch chan<- *Message, wg *sync.WaitGroup) {
+	time.Sleep(4 * time.Second)
+
+	ch <- &Message{
+		chats: []string{
+			"hello",
+			"there",
+			"jane",
+			"you are",
+			"great",
+		},
+	}
+	wg.Done()
+}
+
+func getUserFriends(id string, ch chan<- *Message, wg *sync.WaitGroup) {
+	time.Sleep(2 * time.Second)
+
+	ch <- &Message{
+		friends: []string{
+			"emma",
+			"alex",
+			"jane",
+			"armin",
+			"sasha",
+		},
+	}
+	wg.Done()
 }
